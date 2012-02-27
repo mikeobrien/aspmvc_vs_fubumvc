61ba796f861ba8aff76a6ca2956bbea8a1b4ccd8
mob <mob@mikeobrien.net>
2012-02-27 00:03:58 -0500

    Added the newtonsoft json serializer and create an extension method to serialize an object with javascript dates instead of date strings. Modified to the bootstrapping to use this new method.

---
+++ src/Core/Infrastructure/Web/ObjectExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Core.Infrastructure.Web
+{
+    public static class ObjectExtensions
+    {
+        public static string ToJavaScript(this object source)
+        {
+            return JsonConvert.SerializeObject(source, new JavaScriptDateTimeConverter());
+        }
+    }
+}
